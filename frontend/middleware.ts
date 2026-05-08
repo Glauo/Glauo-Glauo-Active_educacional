93a6c0a9e4afbbac1d81d0a3e824ec7e026b9748
Glauco <gllavolpini@gmail.com>
2026-05-08 16:57:29 -0300

    fix: exclui limpeza estatica do middleware

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -40,5 +40,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|logo.png|manifest.json|sw.js|icons/).*)"]
+  matcher: ["/((?!_next/static|_next/image|favicon.ico|logo.png|manifest.json|sw.js|limpar-cache.html|icons/).*)"]
 };
